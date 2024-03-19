4d64eee9c9da91a52edd92098a2f62f9b8b5dc9d
unnath123 <unnathpgowda@gmail.com>
2024-03-20 00:05:29 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react'
 import Header from './components/header'
 import Main from './components/Main'
 import LoginPage from './components/loginPage'
+import RegisterPage from './components/RegisterPage'
 
 const App = () => {
   return (
     <div>
         {/* <Header/>
         <Main/> */}
-        <LoginPage/>
+        {/* <LoginPage/> */}
+        <RegisterPage/>
     </div>
   )
 }
--- src/components/loginPage/index.jsx
+++ src/components/loginPage/index.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 import loginPic from '../../images/loginPagePic.jpg'
 import EmailIcon from '@mui/icons-material/Email';
 import LockIcon from '@mui/icons-material/Lock';
+import axios from 'axios'
 
 const LoginPage = () =>{
+    const url = 'https://blogprojectbackend.onrender.com/auth/login';
+
+    const [loginID, setLoginId] = useState("");
+    const [password, setPassword] = useState();
+
+   async function handleSubmit(e){
+        e.preventDefault();
+        try{
+            const response = await axios.post(url, {loginID, password})
+            console.log(response.data)
+            console.log("hi")
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+
     return (
         <div className='Loginpage-container'>
             <div className="loginPage-left d-flex">
@@ -12,18 +30,18 @@ const LoginPage = () =>{
             </div>
             <div className="loginPage-right d-flex">
                 <div className='login-form'>
-                    <form action="">
+                    <form>
                         <div className='input-container d-flex'>
                             <EmailIcon/>
-                            <input type="text" placeholder='Email' />
+                            <input type="text" placeholder='Username' name="loginID" onChange={(e)=>setLoginId(e.target.value)}/>
                         </div>
                         <div className='input-container d-flex'>
                             <LockIcon/>
-                            <input className='abcs' type="text" placeholder='Password' />
+                            <input type="password" placeholder='Password' name="password" onChange={(e)=>setPassword(e.target.value)}/>
                         </div>
-                        <button type='submit'>LOGIN</button>
-                        <p style={{marginTop:"0.5rem", textAlign:"center"}}>Forgot <a href="#">Email/Password</a> ?</p>
+                        <button onClick={handleSubmit}>LOGIN</button>
                     </form>
+                    <p style={{marginTop:"0.5rem", textAlign:"center"}}>Forgot <a href="#">Email/Password</a> ?</p>
                 </div>
             </div>
         </div>
